afcb56750dda2834f104b5005d62f2b0e24c0353
Ben DeBoer <bitbucket@eldeboero.com>
2018-12-20 18:11:11 -0800

    Updated to support Maven

---
+++ src/main/java/com/eldeboero/lab/usngcalc/Calculator.java
@@ -0,0 +1,42 @@
+package com.eldeboero.lab.usngcalc;
+import java.util.*;
+import java.lang.*;
+
+// This is the class that does the actual work
+
+public class Calculator {
+
+    double myNorthing;
+    double myEasting;
+
+    double getNorthing() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Northing coordinate:");
+        return input.nextDouble();
+    }
+
+    void setNorthing(double northing) {
+        System.out.println("Using the following value of Northing: " + northing);
+        myNorthing = northing;
+    }
+
+    double getEasting() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Easting coordinate:");
+        return input.nextDouble();
+    }
+
+    void setEasting(double easting) {
+        System.out.println("Using the following value of Easting: " + easting);
+        myEasting = easting;
+    }
+
+    double getResult() {
+
+        double northingSquared;
+        double eastingSquared;
+        double hypotenuse;
+        return Math.sqrt((myNorthing * myNorthing) + (myEasting * myEasting));
+//        return myNorthing / myEasting;
+    } 
+}
